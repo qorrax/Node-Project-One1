3b2f7cb2521b5f5a8a636a3206419293de3a49da
unknown <simplecarrier101@gmail.com>
2023-09-06 21:34:26 -0700

    second commit

--- api/server.js
+++ api/server.js
@@ -4,6 +4,8 @@ import express from 'express';
 
 const app = express();
 
+import {find, findById, insert, update, remove} from './users/model.js';
+
 
 
 
@@ -14,13 +16,29 @@ const app = express();
 
     res.status(200).json({ message: "GET all users" });
 
+
 });
 
 
 
 // GET USER BY ID
 
+app.get('/api/users/:id', (req, res) => {
+    
+        res.status(200).json({ message: "GET user by id" });
+
+});
+
+
+
 // CREATE A NEW USER
+
+app.post('/api/users', (req, res) => {
+        
+            res.status(200).json({ message: "POST new user" });
+    
+    });
+    
 
 // UPDATE A USER
 
