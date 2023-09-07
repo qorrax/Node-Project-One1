0815af5490d72bad84a02eb6ec1072c7d64b1db2
unknown <simplecarrier101@gmail.com>
2023-09-06 20:46:47 -0700

    first  commit

--- api/server.js
+++ api/server.js
@@ -1,6 +1,22 @@
 // Initialize express app
 
+import express from 'express';
+
+const app = express();
+
+
+
+
+
 // GET ALL USERS
+
+ app.get('/api/users', (req, res) => {
+
+    res.status(200).json({ message: "GET all users" });
+
+});
+
+
 
 // GET USER BY ID
 
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // import your node server here
+
+
 import app from './api/server.js';
 
 const port = 9000;
 
 // START YOUR SERVER HERE
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
 
