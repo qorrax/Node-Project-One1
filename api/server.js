350ee537625c0618cb785a309b4d03c123835961
unknown <simplecarrier101@gmail.com>
2023-09-07 22:47:55 -0700

    created some end points

--- api/server.js
+++ api/server.js
@@ -12,24 +12,29 @@ import {find, findById, insert, update, remove} from './users/model.js';
 
 // GET ALL USERS
 
- app.get('/api/users', (req, res) => {
-
-    res.status(200).json({ message: "GET all users" });
-
-
-});
+ app.get('/api/users',  async(req, res) => {
+       const allUsers = await find();
+        res.status(200).json(allUsers);
+        });
 
 
 
 // GET USER BY ID
 
-app.get('/api/users/:id', (req, res) => {
+app.get('/api/users/:id',  async(req, res) => {
     
-        res.status(200).json({ message: "GET user by id" });
+      const user = await findById(req.params.id);
+      if(users) {
 
-});
+        res.status(200).json(user);
 
+        } 
+        else {
+                res.status(404).json({ message: "The user with the specified ID does not exist" });
+                }
+        });
 
+        
 
 // CREATE A NEW USER
 
--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const port = 9000;
 
 // START YOUR SERVER HERE
 
+app.get('/hello', (req, res) => {
+
+    res.json({ message: "Hello World" });
+
+});
+
+
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
